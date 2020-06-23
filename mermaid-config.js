9401be8b71231609c9a0dc33241e41968cf28158
John D. Lewis <jolewis@ddn.com>
2020-06-23 14:23:14 -0400

    Improved colors for Jira statuses

--- mermaid-config.js
+++ mermaid-config.js
@@ -1,12 +1,12 @@
 module.exports = {
     formats: {
-        "Icebox":      "fill:#eee,color:#000",
-        "In Progress": "fill:#cfc,color:#000",
-        "In Review":   "fill:#aca,color:#000",
-        "Defined":     "fill:#ccc,color:#000",
+        "Icebox":      "fill:#ccc,color:#000",
+        "In Progress": "fill:#84a98c,color:#000",
+        "In Review":   "fill:#b4d9bc,color:#000",
+        "Defined":     "fill:#fff,color:#000",
         "Done":        "fill:#00f,color:#fff",
-        "Dead":        "fill:#333,color:#fff",
-        "Emergency":   "fill:#a00,color:#000,stroke-width:3px",
-        "Blocked":     "fill:#faa,color:#000,stroke-width:3px",
+        "Dead":        "fill:#000,color:#fff",
+        "Emergency":   "fill:#95190c,color:#fff",
+        "Blocked":     "fill:#e3b505,color:#fff",
     }
 }
