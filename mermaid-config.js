5030981d0b85de3e356ca0bd89873862327d287b
John D. Lewis <jolewis@ddn.com>
2020-06-23 13:05:18 -0400

    Cleaned up Mermaid colors

--- mermaid-config.js
+++ mermaid-config.js
@@ -1,12 +1,12 @@
 module.exports = {
     formats: {
-        "Icebox":      "fill:#eee,stroke:#eee,color:#000",
-        "In Progress": "fill:#cfc,stroke:#cfc",
-        "In Review":   "fill:#aca",
-        "Defined":     "fill:#ccc",
-        "Done":        "fill:#00f,stroke:#fff,color:#fff",
-        "Dead":        "fill:#333,stroke:#fff,color:#fff",
-        "Emergency":   "fill:#a00,stroke:#000,color:#000,stroke-width:3px",
-        "Blocked":     "fill:#faa,stroke:#000,color:#000,stroke-width:3px",
+        "Icebox":      "fill:#eee,color:#000",
+        "In Progress": "fill:#cfc,color:#000",
+        "In Review":   "fill:#aca,color:#000",
+        "Defined":     "fill:#ccc,color:#000",
+        "Done":        "fill:#00f,color:#fff",
+        "Dead":        "fill:#333,color:#fff",
+        "Emergency":   "fill:#a00,color:#000,stroke-width:3px",
+        "Blocked":     "fill:#faa,color:#000,stroke-width:3px",
     }
 }
