f32fd98938944e0c9d80d4a0e5fea38171021af5
John D. Lewis <jolewis@ddn.com>
2020-11-23 09:01:22 -0500

    Added new script to pull this month's stats

---
+++ pullDataForThisMonth.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+array=("ICEBOX" "DEFINED" "IN PROGRESS" "IN REVIEW" "DONE" "EMERGENCY" "BLOCKED" "DEAD")
+
+today_year=`date '+%Y'`;
+today_mon=`date '+%m'`;
+today_day=`date '+%d'`;
+
+yesterday_year=`date -d "Yesterday 13:00" '+%Y'`
+yesterday_mon=`date -d "Yesterday 13:00" '+%m'`
+yesterday_day=`date -d "Yesterday 13:00" '+%d'`
+
+echo "Fetching all records for this month"
+
+for d in $(seq -f "%02g" 1 ${yesterday_day})
+do
+    for entry in "${array[@]}"
+    do
+	    echo "...$entry $d..."
+	    `node getIssuesByStatusOnDate -s "$entry" -y $yesterday_year -m $yesterday_mon -d $d > "./data/$entry-$yesterday_year-$yesterday_mon-$d.json"`
+	    sleep 3
+    done
+done
