ea6c753369c9de0bc207f6549e5a3aa6b5aeffb0
John D. Lewis <jolewis@ddn.com>
2021-02-05 19:07:00 -0500

    Added per-component team forecast

--- JiraStatusServer.js
+++ JiraStatusServer.js
@@ -2243,7 +2243,12 @@ server.get('/burndown/:rel', async (req, res, next) => {
   let release = req.params.rel ? req.params.rel : false
   let component = req.query.component ? req.query.component : false
   let forecast = (req.query.forecast && req.query.forecast === "yes") ? req.query.forecast : false
-  let efficiency = (req.query.efficiency ? req.query.efficiency : config.forecast.efficiency ? config.forecast.efficiency : 1)
+  let efficiency = (req.query.efficiency ? req.query.efficiency : config.forecast && config.forecast.efficiency && !isNaN(config.forecast.efficiency) ? config.forecast.efficiency : 1)
+
+  let teamSize = config.has('forecast') && config.forecast.has('teamSize') ? config.forecast.teamSize : false
+  if (component) { 
+    teamSize = config.reports.componentTeams[component] 
+  }
 
   let jsrCLM = await jsr.getChartLinkMaker(config).reset()
 
@@ -2329,7 +2334,15 @@ server.get('/burndown/:rel', async (req, res, next) => {
     // debug(`lastTotalEstimate: ${lastTotalEstimate}`)
 
     data['Forecast'] = Array(burndown.dates.length)
-    let hasForecast2 = component ? false : config.has('forecast') && config.forecast.has('teamSize') && typeof config.forecast.teamSize == 'number'
+    // let hasForecast2 = component ? false : config.has('forecast') && config.forecast.has('teamSize') && typeof config.forecast.teamSize == 'number'
+    let hasForecast2 = teamSize > 0
+    if (component) {
+      debug(`hasForecast2: with component ${component}: ${config.has('forecast')} && ${config.has('reports')} && ${config.reports.has('componentTeams')} && ${config.reports.componentTeams.has(component)} && ${typeof config.reports.componentTeams[component]} == 'number'`)
+      hasForecast2 = config.has('forecast') && config.has('reports') && config.reports.has('componentTeams') && config.reports.componentTeams.has(component) && typeof config.reports.componentTeams[component] == 'number'
+    } else { // Team forecast
+      // debug(`hasForecast2 without component: ${config.has('forecast')} && ${config.forecast.has('teamSize')} && ${typeof config.forecast.teamSize} == 'number'`)
+      hasForecast2 = config.has('forecast') && config.forecast.has('teamSize') && typeof config.forecast.teamSize == 'number'
+    }
     debug(`hasForecast2 = ${hasForecast2}`)
 
     if (hasForecast2) {
@@ -2377,7 +2390,11 @@ server.get('/burndown/:rel', async (req, res, next) => {
         }
         
         if (hasForecast2) {
-          currEstimate2 -= Math.round(config.forecast.teamSize * efficiency)
+          if (component) {
+            currEstimate2 -= Math.round(config.reports.componentTeams[component] * efficiency)
+          } else { // For the whole team
+            currEstimate2 -= Math.round(config.forecast.teamSize * efficiency)
+          }
           if (currEstimate2 < 0) {
             currEstimate2 = 0
           }
@@ -2393,14 +2410,14 @@ server.get('/burndown/:rel', async (req, res, next) => {
     // so extend the X axis as needed
     if (hasForecast2) {
       debug(`finalForecast2Value: `, finalForecast2Value)
-      debug(`Forecast_TeamSize ending with ${finalForecast2Value}; ${Math.round(finalForecast2Value/config.forecast.teamSize)} days`)
+      debug(`Forecast_TeamSize ending with ${finalForecast2Value}; ${Math.round(finalForecast2Value/teamSize)} days`)
 
       let continueForecastExtension = finalForecast2Value > 0
-      while (continueForecastExtension && (finalForecast2Value > (-1 * config.forecast.teamSize))) { 
+      while (continueForecastExtension && (finalForecast2Value > (-1 * teamSize))) { 
         jsrCLM.addCategory(`${lastActualDay.getFullYear()}-0${lastActualDay.getMonth()+1}-${lastActualDay.getDate() > 9 ? lastActualDay.getDate() : `0${lastActualDay.getDate() > 9 ? `0${lastActualDay.getDate()}` : lastActualDay.getDate()}`}`)
         loc += 1
         if (lastActualDay.getDay() > 0 && lastActualDay.getDay() < 6) { // Weekday
-          finalForecast2Value -= Math.round(config.forecast.teamSize * efficiency)
+          finalForecast2Value -= Math.round(teamSize * efficiency)
           data['Forecast_TeamSize'][loc] = Math.max(Math.round(finalForecast2Value), 0)
           debug(`setting data['Forecast_TeamSize'][${loc}] to ${data['Forecast_TeamSize'][loc]} on ${lastActualDay}`)
           // data['x'][loc] = lastActualDay
@@ -2439,7 +2456,7 @@ server.get('/burndown/:rel', async (req, res, next) => {
     res.write(`<li><em>Last Estimate (Total):</em> ${lastTotalEstimate} days</li>`)
     res.write(`<li><em>Burndown Rate:</em> ${burndownRate} estDays/day</li>`)
     if (hasForecast2) {
-      res.write(`<li><em>Forecast_TeamSize:</em> ${Math.round(config.forecast.teamSize*efficiency)} estDays/day (Done: ${lastActualDay.getFullYear()}-0${lastActualDay.getMonth()+1}-${lastActualDay.getDate() > 9 ? lastActualDay.getDate() : `0${lastActualDay.getDate()}`}; Team Size: ${config.forecast.teamSize} people; Efficiency Score: ${efficiency})</li>`)
+      res.write(`<li><em>Forecast_TeamSize:</em> ${Math.round(teamSize*efficiency)} estDays/day (Done: ${lastActualDay.getFullYear()}-0${lastActualDay.getMonth()+1}-${lastActualDay.getDate() > 9 ? lastActualDay.getDate() : `0${lastActualDay.getDate()}`}; Team Size: ${teamSize} people; Efficiency Score: ${efficiency})</li>`)
     }
     res.write(`</ul>`)
   } else { // No release date, so stop the chart at the end of the actual estimates
