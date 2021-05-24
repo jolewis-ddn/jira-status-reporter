c90048f91e5965a31ee60b743ce7134905f6884a
John D. Lewis <31732476+jolewis-ddn@users.noreply.github.com>
2021-05-24 11:57:37 -0400

    Added missing story-stats table to jira-stats.sql

--- jira-stats.sql
+++ jira-stats.sql
@@ -6,3 +6,13 @@ CREATE TABLE IF NOT EXISTS "jira-stats" (
         "count" INTEGER NOT NULL,
         PRIMARY KEY("status","year","month","day","count")
 );
+
+CREATE TABLE IF NOT EXISTS "story-stats" (
+        "key"   TEXT NOT NULL,
+        "date"  TEXT NOT NULL,
+        "status"        TEXT NOT NULL,
+        "fixVersion"    TEXT NOT NULL DEFAULT 'NONE',
+        "component"     TEXT,
+        "progress"      INTEGER NOT NULL DEFAULT 0,
+        "total" INTEGER NOT NULL DEFAULT 0
+);
